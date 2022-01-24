ecbb71541904edd691f59ef08c61d6223b36f6fe
Walid Wazini <wazini25@gmail.com>
2022-01-24 15:50:30 +0800

    input UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Box } from '@mui/material';
-;
-
-
-
+import {
+  Container, Box, Input, Row,
+  TextField, Card, CardContent, Button,
+} from '@mui/material';
 function App() {
   return (
     <div className="App">
@@ -25,10 +24,13 @@ function App() {
           Course Goals
         </p>
       </div>
-      <Container fixed
-        style={{ 
-          backgroundColor: '#223252', 
-          height: '100vh', 
+      <Container maxWidth='xl'
+        sx={{
+          backgroundColor: '#223252',
+          height: '100vh',
+          color: 'white',
+          display: 'flex',
+          flexDirection: 'column',
         }}
       >
         <a
@@ -39,8 +41,71 @@ function App() {
         >
           Learn React
         </a>
-      </Container>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }} >
+          <Card
+            // component='form'
+            sx={{
+              mt: 1,
+              boxShadow: '2px black',
+              minWidth: 275,
+              width: '500px'
+              // alignItems: 'center',
+              // height: '200px',
+            }}
+          >
+            <CardContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <p
+                style={{ fontSize: 22, }}
+              >Enter your goals</p>
+              <TextField
+                id='outlined-required'
+                label='Your Goal'
+                size='small'
+                sx={{
+                  backgroundColor: 'whitesmoke',
+                  border: 'none',
+                  borderColor: 'transparent',
+                  borderRadius: '13px',
+                  mb: 3,
+                }}
+              />
+              <div
+                style={{
+                  display: 'flex', flexDirection: 'row',
+                  justifyContent: 'center'
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: '#101b7c',
+                    width: '200px'
+                  }}
+                >Submit
+                </Button>
 
+              </div>
+
+            </CardContent>
+          </Card>
+        </div>
+
+
+        {/* <Input
+          size='small'
+          sx={{
+            backgroundColor: 'white'
+          }} /> */}
+      </Container>
     </div>
   );
 }
